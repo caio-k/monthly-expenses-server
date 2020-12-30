33adc55c6c3bafcfbc65674499e650add2e279c2
caio-k <koiti2000@hotmail.com>
2020-12-30 09:29:21 -0300

    remove useless code

--- src/main/java/com/monthlyexpenses/server/service/InitialMoneyService.java
+++ src/main/java/com/monthlyexpenses/server/service/InitialMoneyService.java
@@ -11,8 +11,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
