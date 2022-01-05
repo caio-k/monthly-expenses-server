40c5f47d5c9184e1b8aba61c8b6f490a5fdc9c53
caio-k <koiti2000@hotmail.com>
2022-01-05 16:54:49 -0300

    add configuration annotation for unit test

---
+++ src/test/java/com/monthlyexpenses/server/BasicConfigurationTest.java
@@ -0,0 +1,9 @@
+package com.monthlyexpenses.server;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = {MessagesComponent.class, MessagesConfig.class})
+public class BasicConfigurationTest {
+}
